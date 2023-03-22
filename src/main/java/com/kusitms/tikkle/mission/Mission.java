cbb56266e0d1ab04eaed5e6dc7421fe8b2629702
u__j <20201111@duksung.ac.kr>
2023-03-22 15:10:37 +0900

    Fix : Mission.java에 @Entity 어노테이션 추가

--- src/main/java/com/kusitms/tikkle/mission/Mission.java
+++ src/main/java/com/kusitms/tikkle/mission/Mission.java
@@ -6,14 +6,13 @@ import com.kusitms.tikkle.participate.Participate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Setter
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Mission {
