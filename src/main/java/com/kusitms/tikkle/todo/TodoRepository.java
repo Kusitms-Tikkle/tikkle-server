6e4228729129b1864e4668240f9dfb2869e270d2
u__j <20201111@duksung.ac.kr>
2023-09-14 14:23:39 +0900

    feat : 메모 작성 안한 Todo 조회 api #76

--- src/main/java/com/kusitms/tikkle/todo/TodoRepository.java
+++ src/main/java/com/kusitms/tikkle/todo/TodoRepository.java
@@ -1,10 +1,19 @@
 package com.kusitms.tikkle.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findByAccountIdAndDate(Long id, String date);
     List<Todo> findByDate(String date);
+
+    @Query(value = "SELECT * FROM Todo t " +
+            "LEFT JOIN Memo m ON t.todo_id = m.todo_id " +
+            "WHERE m.memo_id IS NULL " +
+            "AND t.date = :date " +
+            "AND t.account_id = :account_id", nativeQuery = true)
+    List<Todo> findUnWrittedTodoByAccountIdAndDate(@Param("account_id") Long id, @Param("date") String date);
 }
---
+++ src/main/java/com/kusitms/tikkle/todo/dto/todoResWithMemo.java
@@ -0,0 +1,13 @@
+package com.kusitms.tikkle.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class todoResWithMemo {
+    public Long id;
+    public String title;
+}
